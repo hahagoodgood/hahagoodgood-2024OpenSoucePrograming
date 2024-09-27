3bdfe6b016114223190b789b07d1f952f160e77b
DHKim <dh95229505@gmail.com>
2024-09-27 12:24:24 +0900

    V0.3 Type

--- week05/main.go
+++ week05/main.go
@@ -1,9 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"reflect"
+	"strings"
+)
 
 func main() {
-	var i int = 55
+	// var i int = 55
+
+	// var i int
+	// i = 55
+
+	f := 4.30
+	i := 55
+
+	// var f float32 = 4.30
+
+	fmt.Println(reflect.TypeOf(f), reflect.TypeOf(i))
+	fmt.Printf("%s\n", strings.Title("Kim inha"))
+
+	fmt.Println(math.Ceil(3.99))
+
 	fmt.Printf("value i : %d\n", i)
 	fmt.Print("value i: ", i, "\n")
 	fmt.Println("value i: ", i)
