c22390b54eb976cc770a8c570c5f9894b38d742d
DHKim <dh95229505@gmail.com>
2024-10-04 10:35:49 +0900

    v0.4 converstion

---
+++ week06/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	// var i int = 55
+
+	// var i int
+	// i = 55
+
+	f := 12.9
+	i := 55
+
+	fmt.Printf("value i : %d\n", i)
+	// fmt.Printf("%d + %f = %f\n", i, f, i*f)
+	// .\main.go:17:37: invalid operation: i * f (mismatched types int and float64)
+	// fmt.Printf("%d + %f = %d\n", i, f, i*int(f)) //conversion
+	fmt.Printf("%d + %f = %f\n", i, f, float64(i)*f) //conversion
+	fmt.Printf("%d\n", int(f))
+	fmt.Println(reflect.TypeOf(i))
+}
