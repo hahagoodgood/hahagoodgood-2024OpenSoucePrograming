d2794181e01267f3f3f30c7d109f1c6b42306ccd
DHKim <dh95229505@gmail.com>
2024-10-04 10:39:54 +0900

    v0.5 Conversion

--- week06/main.go
+++ week06/main.go
@@ -11,14 +11,14 @@ func main() {
 	// var i int
 	// i = 55
 
-	f := 12.9
+	var f float64 = 12.9
 	i := 55
 
 	fmt.Printf("value i : %d\n", i)
 	// fmt.Printf("%d + %f = %f\n", i, f, i*f)
 	// .\main.go:17:37: invalid operation: i * f (mismatched types int and float64)
-	// fmt.Printf("%d + %f = %d\n", i, f, i*int(f)) //conversion
-	fmt.Printf("%d + %f = %f\n", i, f, float64(i)*f) //conversion
-	fmt.Printf("%d\n", int(f))
-	fmt.Println(reflect.TypeOf(i))
+	fmt.Printf("%d + %f = %d\n", i, f, i*int(f)) //conversion
+	// fmt.Printf("%d + %f = %f\n", i, f, float64(i)*f) //conversion
+	// fmt.Printf("%d\n", int(f))
+	fmt.Println(reflect.TypeOf(i), reflect.TypeOf(f))
 }
