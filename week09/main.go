233b37ae4ee89abb232e416f26e9a9db64465123
DHKim <dh95229505@gmail.com>
2024-10-25 11:24:20 +0900

    V1.9 Guess Game Final

--- week09/main.go
+++ week09/main.go
@@ -85,6 +85,8 @@ func main() {
 }
 */
 
+/*
+// V1.8 Guess Game
 import (
 	"bufio"
 	"fmt"
@@ -127,4 +129,57 @@ func main() {
 		}
 	}
 
+}
+
+
+*/
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func main() {
+	rand.Seed(time.Now().Unix())
+	answer := rand.Intn(6) + 1
+	fmt.Printf("%d\n", answer)
+
+	win := false
+	//for 문을 이용하여 게임의 여러 기회 제공
+	for guesses := 0; guesses < 3; guesses++ {
+		fmt.Printf("%d번의 기회가 남았습니다. 숫자 입력 : ", 3-guesses)
+		r := bufio.NewReader(os.Stdin)
+		i, err := r.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		i = strings.TrimSpace(i)
+		guess, err := strconv.Atoi(i)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(guess)
+
+		if answer == guess {
+			fmt.Println("정답입니다!")
+			win = true
+			break
+		} else if answer > guess {
+			fmt.Println("입력하신 수는 정답보다 작은 수 입니다. LOW")
+		} else {
+			fmt.Println("입력하신 수는 정답보다 큰 수 입니다. HIGH")
+		}
+	}
+	if win {
+		fmt.Printf("당신이 이겼습니다.")
+	} else {
+		fmt.Printf("당신이 졌습니다.")
+	}
 }
