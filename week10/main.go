d9667c3705e733473cb393d2ccad6cc46f287a95
DHKim <dh95229505@gmail.com>
2024-11-01 11:46:00 +0900

    v2.9 PrimeNumber

--- week10/main.go
+++ week10/main.go
@@ -81,7 +81,7 @@ func main() {
 	}
 } */
 
-import (
+/* import (
 	"bufio"
 	"fmt"
 	"log"
@@ -109,6 +109,54 @@ func main() {
 		isPrime = false
 	} else {
 		for j := 2; j <= int(math.Sqrt(float64(n))); j++ { //2부터 입력된 수까지 반복
+			if n%j == 0 { //약수면
+				//counts++ //나누어 떠러지는 횟수 카운트
+				isPrime = false
+				break // 무의미한 반복 제거
+			}
+			fmt.Printf("%d ", j)
+		}
+	}
+
+	// if counts == 0 { //나누어 떨어지는 수가 있으면 안됩
+	if isPrime {
+		fmt.Printf("%d는(은) 소수입니다.", n)
+	} else {
+		fmt.Printf("%d는(은) 소수가 아닙니다.", n)
+	}
+} */
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fmt.Print("정수입력 : ")
+	r := bufio.NewReader(os.Stdin)
+	i, err := r.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	i = strings.TrimSpace(i)
+	n, err := strconv.Atoi(i)
+
+	var isPrime bool = true // 가독성 계선
+
+	// 논리 해결
+	if n < 2 { // 1보다 큰 자연수 중(!!!) 1과 자기 자신만을 약수로 가지는 수
+		isPrime = false
+	} else if n == 2 {
+		isPrime = true
+
+	} else if n%2 == 0 {
+		isPrime = false
+	} else {
+		for j := 3; j*j <= n; j += 2 { //2부터 입력된 수까지 반복
 			if n%j == 0 { //약수면
 				//counts++ //나누어 떠러지는 횟수 카운트
 				isPrime = false
