6943c47edea48cd49f0309f25d9ddb7700222f50
DHKim <dh95229505@gmail.com>
2024-11-01 10:42:16 +0900

    v2.3 Prime number

--- week10/main.go
+++ week10/main.go
@@ -1,5 +1,49 @@
 package main
 
+// 1이 소수인 문제 발생
+/* import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// func paintNeeded(width float64, height float64  ){
+
+// }
+
+// func main(){
+// 	amount, err := paintNeeded(5.2, 3.5)
+// }
+
+func main() {
+	fmt.Print("정수입력 : ")
+	r := bufio.NewReader(os.Stdin)
+	i, err := r.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	i = strings.TrimSpace(i)
+	n, err := strconv.Atoi(i)
+
+	counts := 0
+	for j := 1; j <= n; j++ { //1부터 입력된 수까지 반복
+		if n%j == 0 { //약수면
+			counts++ //나누어 떠러지는 횟수 카운트
+		}
+	}
+
+	if counts == 2 {
+		fmt.Printf("%d는(은) 소수입니다.", n)
+	} else {
+		fmt.Printf("%d는(은) 소수가 아닙니다.", n)
+	}
+} */
+
+
 import (
 	"bufio"
 	"fmt"
@@ -29,13 +73,13 @@ func main() {
 	n, err := strconv.Atoi(i)
 
 	counts := 0
-	for j := 1; j <= n; j++ { //1입력된 수까지 반복
+	for j := 2; j <= n; j++ { //2부터 입력된 수까지 반복
 		if n%j == 0 { //약수면
 			counts++ //나누어 떠러지는 횟수 카운트
 		}
 	}
 
-	if counts == 2 {
+	if counts == 0 { //나누어 떨어지는 수가 있으면 안됩
 		fmt.Printf("%d는(은) 소수입니다.", n)
 	} else {
 		fmt.Printf("%d는(은) 소수가 아닙니다.", n)
