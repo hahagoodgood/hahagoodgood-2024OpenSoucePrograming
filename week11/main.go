67eae9d0fab06a692c85888924ad07ed616d58d7
kimdonghyuk <144420387+hahagoodgood@users.noreply.github.com>
2024-11-08 11:32:26 +0900

    v3.3 Packge

--- week11/main.go
+++ week11/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "week11/greeting"
+
 // import (
 // 	"bufio"
 // 	"fmt"
@@ -60,65 +62,71 @@ package main
 // 	}
 // }
 
-import (
-	"bufio"
-	"fmt"
-	"log"
-	"os"
-	"strconv"
-	"strings"
-)
+// import (
+// 	"bufio"
+// 	"fmt"
+// 	"log"
+// 	"os"
+// 	"strconv"
+// 	"strings"
+// )
 
-func isPrime(n int) bool {
-	if n < 2 {
-		return false
-	} else if n == 2 {
-		return true
-	} else if n%2 == 0 {
-		return false
-	} else {
-		for j := 3; j*j <= n; j = j + 2 {
-			if n%j == 0 {
-				return false
-			}
-			//fmt.Printf("%d ", j)
-		}
-	}
-	return true
-}
+// func isPrime(n int) bool {
+// 	if n < 2 {
+// 		return false
+// 	} else if n == 2 {
+// 		return true
+// 	} else if n%2 == 0 {
+// 		return false
+// 	} else {
+// 		for j := 3; j*j <= n; j = j + 2 {
+// 			if n%j == 0 {
+// 				return false
+// 			}
+// 			//fmt.Printf("%d ", j)
+// 		}
+// 	}
+// 	return true
+// }
 
-func getInteger() (int, error) {
-	fmt.Print("첫 번째 정수(시작 값) 입력 : ")
-	r := bufio.NewReader(os.Stdin)
-	a, err := r.ReadString('\n')
-	if err != nil {
-		return 0, err
-	}
+// func getInteger() (int, error) {
+// 	fmt.Print("첫 번째 정수(시작 값) 입력 : ")
+// 	r := bufio.NewReader(os.Stdin)
+// 	a, err := r.ReadString('\n')
+// 	if err != nil {
+// 		return 0, err
+// 	}
 
-	a = strings.TrimSpace(a)
-	n, err := strconv.Atoi(a)
-	if err != nil {
-		return 0, err
-	}
-	return n, nil
-}
+// 	a = strings.TrimSpace(a)
+// 	n, err := strconv.Atoi(a)
+// 	if err != nil {
+// 		return 0, err
+// 	}
+// 	return n, nil
+// }
+
+// func main() {
+// 	fmt.Print("첫 번째 정수(시작 값) 입력 : ")
+// 	n1, err := getInteger()
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	fmt.Print("두 번째 정수(끝 값) 입력 : ")
+// 	n2, err := getInteger()
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	for i := n1; i <= n2; i++ {
+// 		if isPrime(i) {
+// 			fmt.Printf("%d ", i)
+// 		}
+// 	}
+// }
 
 func main() {
-	fmt.Print("첫 번째 정수(시작 값) 입력 : ")
-	n1, err := getInteger()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Print("두 번째 정수(끝 값) 입력 : ")
-	n2, err := getInteger()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for i := n1; i <= n2; i++ {
-		if isPrime(i) {
-			fmt.Printf("%d ", i)
-		}
-	}
+	// greeting.Hi("기미나")
+	// greeting.Hello("하버드")
+	greeting.AllGreeting("카리나")
 }
