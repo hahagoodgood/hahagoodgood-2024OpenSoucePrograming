150238bffb8306eebf504def1fc804ff14a2ecab
kimdonghyuk <144420387+hahagoodgood@users.noreply.github.com>
2024-11-15 12:20:37 +0900

    v4.2 slice

--- week12/main.go
+++ week12/main.go
@@ -2,8 +2,7 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/headfirstgo/keyboard"
+	"reflect"
 )
 
 func main() {
@@ -36,13 +35,24 @@ func main() {
 	// 	fmt.Println(i, date)
 	// }
 
-	var gpa [3]float64
-	// go get github.com/headfirstgo.keyboard
-	for i := 0; i < len(gpa); i++ {
-		fmt.Print("Input fleat number :")
-		gpa[i], _ = keyboard.GetFloat()
-	}
-	for index, value := range gpa {
-		fmt.Printf("%d %f\n", index, value)
-	}
+	// var gpa [3]float64
+	// // go get github.com/headfirstgo.keyboard
+	// for i := 0; i < len(gpa); i++ {
+	// 	fmt.Print("Input fleat number :")
+	// 	gpa[i], _ = keyboard.GetFloat()
+	// }
+	// for index, value := range gpa {
+	// 	fmt.Printf("%d %f\n", index, value)
+	// }
+
+	// 파일 입출력
+	// os.open()
+
+	// 슬라이스
+	// 슬라이스와 배열의 차이점이 뭐지?
+	var gpa [5]float64 = [5]float64{3.5, 4.1, 4.5, 3.9, 4.25}
+	gpa_slice := gpa[1:4]
+	fmt.Println(gpa_slice, reflect.TypeOf(gpa_slice))
+	fmt.Println(gpa, reflect.TypeOf(gpa))
+
 }
