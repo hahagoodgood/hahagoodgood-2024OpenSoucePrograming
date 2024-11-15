6ca114e09707236cacc74e2777eebc61bc29b8d2
kimdonghyuk <144420387+hahagoodgood@users.noreply.github.com>
2024-11-15 11:21:15 +0900

    v3.8 array

--- week12/main.go
+++ week12/main.go
@@ -15,8 +15,8 @@ func main() {
 	// dates := [3]time.Time{time.Unix(1, 0), time.Unix(0, 0), time.Unix(147920001, 0)}
 
 	// dates := [3]time.Time{time.Unix(1, 0),
-		// time.Unix(0, 0),
-		// time.Unix(147920001, 0)}
+	// time.Unix(0, 0),
+	// time.Unix(147920001, 0)}
 
 	// 줄바꿈시 뒤에는 무조건 쉽표가 필요하다
 	dates := [3]time.Time{time.Unix(1, 0),
@@ -24,5 +24,15 @@ func main() {
 		time.Unix(147920001, 0),
 	}
 
-	fmt.Print(dates[0], dates[1], dates[2])
+	// fmt.Println(dates[0], dates[1], dates[2])
+
+	// fmt.Println(dates)
+
+	// fmt.Printf("%#v\n", dates)
+
+
+	// 포이치문 조작 방법!!
+	for i, v := range dates {
+		fmt.Println(i, v)
+	}
 }
