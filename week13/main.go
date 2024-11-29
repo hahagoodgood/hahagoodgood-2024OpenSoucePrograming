967efbf84f0fe0618262a34ed7d72ab9e36d8d5e
kimdonghyuk <144420387+hahagoodgood@users.noreply.github.com>
2024-11-29 11:05:41 +0900

    v4.5 map

--- week13/main.go
+++ week13/main.go
@@ -1,6 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"reflect"
+)
+
+// 항상 가변 매개변수는 가장 오른쪽에 와야한다.
+func test(strs ...string) {
+	fmt.Println(strs, reflect.TypeOf(strs))
+	// strs는 슬라이스다
+}
 
 func main() {
 	// var gpa [5]float64 = [5]float64{3.5, 4.1, 4.5, 3.9, 4.25}
@@ -10,12 +20,12 @@ func main() {
 	// fmt.Println(len(gpa_slice), gpa_slice, gpa)
 
 	// ???
-	s1 := []string{"s1","s1"}
-	s2 := append(s1,"s2","s2")
-	s3 := append(s2, "s3","s3")
-	s4 := append(s3, "s4", "s4")
-	s4[0]= "xx"
-	fmt.Println(s1,s2,s3,s4)
+	// s1 := []string{"s1","s1"}
+	// s2 := append(s1,"s2","s2")
+	// s3 := append(s2, "s3","s3")
+	// s4 := append(s3, "s4", "s4")
+	// s4[0]= "xx"
+	// fmt.Println(s1,s2,s3,s4)
 
 	// s1 := []string{"s1", "s1"}
 	// s1 = append(s1, "s2", "s2")
@@ -36,4 +46,12 @@ func main() {
 	// gpa_slice = append(gpa_slice, 4.3)
 	// fmt.Println(len(gpa_slice), gpa_slice, gpa)
 
+	slice := os.Args[1:]
+	fmt.Println(slice[1])
+	fmt.Printf("%T\n", slice[1])
+	slice = append(slice, "i", "n", "h", "a")
+	fmt.Println(slice)
+
+	test("asdfa", "Asdfads", "Asdfasdf")
+	test("adsfa")
 }
---
+++ week14/main.go
@@ -0,0 +1,76 @@
+package main
+
+import "fmt"
+
+// import (
+// 	"fmt"
+// 	"github.com/headfirstgo/datafile"
+// 	"log"
+// )
+
+// func main() {
+// 	lines, err := datafile.GetStrings("votes.txt")
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	counts := make(map[string]int)
+// 	for _, line := range lines {
+// 		counts[line]++
+// 	}
+// 	for name, count := range counts {
+// 		fmt.Printf("Votes for %s: %d\n", name, count)
+// 	}
+// }
+
+// import (
+// 	"fmt"
+// 	"github.com/headfirstgo/datafile"
+// 	"log"
+// )
+// func main() {
+// 	lines, err := datafile.GetStrings("votes.txt")
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	var names []string
+// 	var counts []int
+// 	for _, line := range lines{
+// 		matched := false
+// 		for i, name := range names{
+// 			if name == line {
+// 				counts[i]++
+// 				matched =true
+// 			}
+// 		}
+// 		if matched == false {
+// 			names = append(names, line)
+// 			counts = append(counts, 1)
+// 		}
+// 	}
+// 	for i, name := range names {
+// 		fmt.Printf("Votes for %s: %d\n", name, counts[i])
+// 	}
+// }
+
+func main(){
+	ages := make(map[string]int)
+
+	var name string
+	var age int
+
+	for {
+		fmt.Print("What's ur name? (exit to 'q)")
+		fmt.Scanln(&name)
+		if name == "q"{
+			break
+		}
+		fmt.Print("Ur age?")
+		fmt.Scanln(&age)
+		ages[name] = age
+	}
+
+	for name, age := range ages{
+		fmt.Printf("%s is %d year old.\n",name, age)
+	}
+}
